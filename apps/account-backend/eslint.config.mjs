a6af8a48ccc7facb786c63f25ad430e8cc33a37e
Yudi <12040397+Yudi@users.noreply.github.com>
2026-06-23 16:53:36 -0300

    Fix lint

--- apps/account-backend/eslint.config.mjs
+++ apps/account-backend/eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'dist/**', 'captcha-training-data/**'],
+    ignores: ['eslint.config.mjs', 'webpack.config.js', 'dist/**', 'captcha-training-data/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
